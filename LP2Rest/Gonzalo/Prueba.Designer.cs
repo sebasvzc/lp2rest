4bdfa66afbf56ba150f6cb0cdf60fc7e020d9320
gonzaquis29 <59510520+gonzaquis29@users.noreply.github.com>
2022-10-25 19:22:56 -0500

    Esto es una prueba

---
+++ LP2Rest/Gonzalo/Prueba.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace LP2Rest.Gonzalo
+{
+    partial class Prueba
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.Location = new System.Drawing.Point(297, 52);
+            this.lblTitulo.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(68, 20);
+            this.lblTitulo.TabIndex = 65;
+            this.lblTitulo.Text = "Prueba";
+            this.lblTitulo.Click += new System.EventHandler(this.lblTitulo_Click);
+            // 
+            // Prueba
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblTitulo);
+            this.Name = "Prueba";
+            this.Text = "Prueba";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitulo;
+    }
+}
--- LP2Rest/Gonzalo/frmListarPlatos.Designer.cs
+++ LP2Rest/Gonzalo/frmListarPlatos.Designer.cs
@@ -30,6 +30,11 @@
         {
             this.btnBuscar = new System.Windows.Forms.Button();
             this.dgvEmpleados = new System.Windows.Forms.DataGridView();
+            this.NombreCompleto = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Teléfono = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.email = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Stock = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Area = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.comboBox1 = new System.Windows.Forms.ComboBox();
             this.label9 = new System.Windows.Forms.Label();
             this.lblTitulo = new System.Windows.Forms.Label();
@@ -42,11 +47,6 @@
             this.Anular = new System.Windows.Forms.ToolStripButton();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.NombreCompleto = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Teléfono = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.email = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Stock = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Area = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.dgvEmpleados)).BeginInit();
             this.toolStrip1.SuspendLayout();
             this.SuspendLayout();
@@ -54,10 +54,10 @@
             // btnBuscar
             // 
             this.btnBuscar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold);
-            this.btnBuscar.Location = new System.Drawing.Point(704, 19);
-            this.btnBuscar.Margin = new System.Windows.Forms.Padding(4);
+            this.btnBuscar.Location = new System.Drawing.Point(864, 21);
+            this.btnBuscar.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.btnBuscar.Name = "btnBuscar";
-            this.btnBuscar.Size = new System.Drawing.Size(141, 28);
+            this.btnBuscar.Size = new System.Drawing.Size(188, 34);
             this.btnBuscar.TabIndex = 132;
             this.btnBuscar.Text = "Buscar";
             this.btnBuscar.UseVisualStyleBackColor = true;
@@ -73,135 +73,14 @@
             this.email,
             this.Stock,
             this.Area});
-            this.dgvEmpleados.Location = new System.Drawing.Point(28, 177);
-            this.dgvEmpleados.Margin = new System.Windows.Forms.Padding(4);
+            this.dgvEmpleados.Location = new System.Drawing.Point(37, 218);
+            this.dgvEmpleados.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
             this.dgvEmpleados.Name = "dgvEmpleados";
             this.dgvEmpleados.ReadOnly = true;
             this.dgvEmpleados.RowHeadersWidth = 51;
             this.dgvEmpleados.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dgvEmpleados.Size = new System.Drawing.Size(761, 220);
+            this.dgvEmpleados.Size = new System.Drawing.Size(1015, 271);
             this.dgvEmpleados.TabIndex = 131;
-            // 
-            // comboBox1
-            // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Location = new System.Drawing.Point(481, 80);
-            this.comboBox1.Margin = new System.Windows.Forms.Padding(4);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(165, 21);
-            this.comboBox1.TabIndex = 130;
-            // 
-            // label9
-            // 
-            this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(55, 121);
-            this.label9.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(47, 13);
-            this.label9.TabIndex = 129;
-            this.label9.Text = "Nombre:";
-            this.label9.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
-            // 
-            // lblTitulo
-            // 
-            this.lblTitulo.AutoSize = true;
-            this.lblTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblTitulo.Location = new System.Drawing.Point(24, 23);
-            this.lblTitulo.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.lblTitulo.Name = "lblTitulo";
-            this.lblTitulo.Size = new System.Drawing.Size(128, 16);
-            this.lblTitulo.TabIndex = 128;
-            this.lblTitulo.Text = "Listado de Platos";
-            // 
-            // txtDNINombre
-            // 
-            this.txtDNINombre.Location = new System.Drawing.Point(133, 77);
-            this.txtDNINombre.Margin = new System.Windows.Forms.Padding(4);
-            this.txtDNINombre.Name = "txtDNINombre";
-            this.txtDNINombre.ReadOnly = true;
-            this.txtDNINombre.Size = new System.Drawing.Size(165, 20);
-            this.txtDNINombre.TabIndex = 118;
-            // 
-            // lblDNINombre
-            // 
-            this.lblDNINombre.AutoSize = true;
-            this.lblDNINombre.Location = new System.Drawing.Point(25, 83);
-            this.lblDNINombre.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.lblDNINombre.Name = "lblDNINombre";
-            this.lblDNINombre.Size = new System.Drawing.Size(75, 13);
-            this.lblDNINombre.TabIndex = 117;
-            this.lblDNINombre.Text = "ID Item Venta:";
-            this.lblDNINombre.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
-            // 
-            // toolStrip1
-            // 
-            this.toolStrip1.BackColor = System.Drawing.SystemColors.ButtonFace;
-            this.toolStrip1.Dock = System.Windows.Forms.DockStyle.None;
-            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
-            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.btnNuevo,
-            this.btnModificar,
-            this.btnEliminar,
-            this.Anular});
-            this.toolStrip1.Location = new System.Drawing.Point(254, 430);
-            this.toolStrip1.Name = "toolStrip1";
-            this.toolStrip1.Size = new System.Drawing.Size(331, 27);
-            this.toolStrip1.TabIndex = 133;
-            this.toolStrip1.Text = "tsMenu";
-            // 
-            // btnNuevo
-            // 
-            this.btnNuevo.Image = global::LP2Rest.Properties.Resources.new_file_40454;
-            this.btnNuevo.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.btnNuevo.Name = "btnNuevo";
-            this.btnNuevo.Size = new System.Drawing.Size(66, 24);
-            this.btnNuevo.Text = "&Nuevo";
-            this.btnNuevo.Click += new System.EventHandler(this.btnNuevo_Click);
-            // 
-            // btnModificar
-            // 
-            this.btnModificar.Image = global::LP2Rest.Properties.Resources.edit_validated_40458;
-            this.btnModificar.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.btnModificar.Name = "btnModificar";
-            this.btnModificar.Size = new System.Drawing.Size(82, 24);
-            this.btnModificar.Text = "&Modificar";
-            // 
-            // btnEliminar
-            // 
-            this.btnEliminar.Image = global::LP2Rest.Properties.Resources.trash_40429;
-            this.btnEliminar.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.btnEliminar.Name = "btnEliminar";
-            this.btnEliminar.Size = new System.Drawing.Size(74, 24);
-            this.btnEliminar.Text = "&Eliminar";
-            // 
-            // Anular
-            // 
-            this.Anular.Image = global::LP2Rest.Properties.Resources._391127;
-            this.Anular.ImageTransparentColor = System.Drawing.Color.Magenta;
-            this.Anular.Name = "Anular";
-            this.Anular.Size = new System.Drawing.Size(66, 24);
-            this.Anular.Text = "&Anular";
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(133, 121);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.ReadOnly = true;
-            this.textBox1.Size = new System.Drawing.Size(165, 20);
-            this.textBox1.TabIndex = 134;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(372, 83);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(59, 13);
-            this.label1.TabIndex = 135;
-            this.label1.Text = "Disponible:";
-            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
-            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // NombreCompleto
             // 
@@ -243,11 +122,132 @@
             this.Area.ReadOnly = true;
             this.Area.Width = 120;
             // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(641, 98);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(219, 24);
+            this.comboBox1.TabIndex = 130;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(73, 149);
+            this.label9.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(59, 16);
+            this.label9.TabIndex = 129;
+            this.label9.Text = "Nombre:";
+            this.label9.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // lblTitulo
+            // 
+            this.lblTitulo.AutoSize = true;
+            this.lblTitulo.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitulo.Location = new System.Drawing.Point(32, 28);
+            this.lblTitulo.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(156, 20);
+            this.lblTitulo.TabIndex = 128;
+            this.lblTitulo.Text = "Listado de Platos";
+            // 
+            // txtDNINombre
+            // 
+            this.txtDNINombre.Location = new System.Drawing.Point(177, 95);
+            this.txtDNINombre.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.txtDNINombre.Name = "txtDNINombre";
+            this.txtDNINombre.ReadOnly = true;
+            this.txtDNINombre.Size = new System.Drawing.Size(219, 22);
+            this.txtDNINombre.TabIndex = 118;
+            // 
+            // lblDNINombre
+            // 
+            this.lblDNINombre.AutoSize = true;
+            this.lblDNINombre.Location = new System.Drawing.Point(33, 102);
+            this.lblDNINombre.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.lblDNINombre.Name = "lblDNINombre";
+            this.lblDNINombre.Size = new System.Drawing.Size(89, 16);
+            this.lblDNINombre.TabIndex = 117;
+            this.lblDNINombre.Text = "ID Item Venta:";
+            this.lblDNINombre.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.toolStrip1.Dock = System.Windows.Forms.DockStyle.None;
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.btnNuevo,
+            this.btnModificar,
+            this.btnEliminar,
+            this.Anular});
+            this.toolStrip1.Location = new System.Drawing.Point(339, 529);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(349, 27);
+            this.toolStrip1.TabIndex = 133;
+            this.toolStrip1.Text = "tsMenu";
+            // 
+            // btnNuevo
+            // 
+            this.btnNuevo.Image = global::LP2Rest.Properties.Resources.new_file_40454;
+            this.btnNuevo.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnNuevo.Name = "btnNuevo";
+            this.btnNuevo.Size = new System.Drawing.Size(76, 24);
+            this.btnNuevo.Text = "&Nuevo";
+            this.btnNuevo.Click += new System.EventHandler(this.btnNuevo_Click);
+            // 
+            // btnModificar
+            // 
+            this.btnModificar.Image = global::LP2Rest.Properties.Resources.edit_validated_40458;
+            this.btnModificar.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnModificar.Name = "btnModificar";
+            this.btnModificar.Size = new System.Drawing.Size(97, 24);
+            this.btnModificar.Text = "&Modificar";
+            // 
+            // btnEliminar
+            // 
+            this.btnEliminar.Image = global::LP2Rest.Properties.Resources.trash_40429;
+            this.btnEliminar.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(87, 24);
+            this.btnEliminar.Text = "&Eliminar";
+            // 
+            // Anular
+            // 
+            this.Anular.Image = global::LP2Rest.Properties.Resources._391127;
+            this.Anular.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.Anular.Name = "Anular";
+            this.Anular.Size = new System.Drawing.Size(76, 24);
+            this.Anular.Text = "&Anular";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(177, 149);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(5, 5, 5, 5);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(219, 22);
+            this.textBox1.TabIndex = 134;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(496, 102);
+            this.label1.Margin = new System.Windows.Forms.Padding(5, 0, 5, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 16);
+            this.label1.TabIndex = 135;
+            this.label1.Text = "Disponible:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
             // frmListarPlatos
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1004, 529);
+            this.ClientSize = new System.Drawing.Size(1339, 651);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.toolStrip1);
@@ -258,6 +258,7 @@
             this.Controls.Add(this.lblTitulo);
             this.Controls.Add(this.txtDNINombre);
             this.Controls.Add(this.lblDNINombre);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "frmListarPlatos";
             this.Text = "frmListarPlatos";
             ((System.ComponentModel.ISupportInitialize)(this.dgvEmpleados)).EndInit();
