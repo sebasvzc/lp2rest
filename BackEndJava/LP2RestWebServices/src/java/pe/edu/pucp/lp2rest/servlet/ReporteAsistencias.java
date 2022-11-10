45e5804e5813b10957e44defb7e46d8b766c994f
gonzaquis29 <59510520+gonzaquis29@users.noreply.github.com>
2022-11-10 02:35:05 -0500

    Reporte Asistencias terminado al 90%, c# no muestra el resumen

--- BackEndJava/LP2RestWebServices/src/java/pe/edu/pucp/lp2rest/servlet/ReporteAsistencias.java
+++ BackEndJava/LP2RestWebServices/src/java/pe/edu/pucp/lp2rest/servlet/ReporteAsistencias.java
@@ -4,12 +4,22 @@
  */
 package pe.edu.pucp.lp2rest.servlet;
 
+import java.awt.Image;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.util.HashMap;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.swing.ImageIcon;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.engine.util.JRLoader;
+import pe.edu.pucp.lp2rest.config.DBManager;
 
 /**
  *
@@ -17,29 +27,25 @@ import javax.servlet.http.HttpServletResponse;
  */
 public class ReporteAsistencias extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        try ( PrintWriter out = response.getWriter()) {
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet ReporteAsistencias</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Servlet ReporteAsistencias at " + request.getContextPath() + "</h1>");
-            out.println("</body>");
-            out.println("</html>");
+        try{
+            Connection con = DBManager.getInstance().getConnection();
+            JasperReport reporte = (JasperReport) JRLoader.loadObject(
+                ReporteAsistencias.class.getResource("/pe/edu/pucp/lp2rest/report/AsistenciasDefinitivo.jasper"));
+            String rutaSubReporte = ReporteAsistencias.class.getResource(
+                    "/pe/edu/pucp/lp2rest/report/ReporteAsistenciasGrafico.jasper").getPath();
+            
+            String rutaImagen = ReporteAsistencias.class.getResource("/pe/edu/pucp/lp2rest/img/LogoCrema.jpg").getPath();
+            Image imagen = (new ImageIcon(rutaImagen)).getImage();
+            HashMap parametros = new HashMap();
+            parametros.put("ImagenLogoEmpresa", imagen);
+            parametros.put("SubReporteGrafico",rutaSubReporte);
+            JasperPrint jp = JasperFillManager.fillReport(reporte,parametros,con);
+            con.close();
+            JasperExportManager.exportReportToPdfStream(jp,response.getOutputStream());
+        }catch(Exception ex){
+            System.out.println(ex.getMessage());
         }
     }
 
