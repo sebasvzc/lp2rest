6a2d88a8e58c08b4c2d19b6d38b68e4e7511d027
GerardTorres1 <a20194793@pucp.edu.pe>
2022-11-22 04:27:46 -0500

    Falta disenio DGV y revision

--- FrontEndCSharp/LP2Rest/Omar/frmReporteOrdenesVenta.Designer.cs
+++ FrontEndCSharp/LP2Rest/Omar/frmReporteOrdenesVenta.Designer.cs
@@ -30,115 +30,101 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmReporteOrdenesVenta));
             this.btnGenerar = new System.Windows.Forms.Button();
-            this.pbPDF = new System.Windows.Forms.PictureBox();
             this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.dtpFechaFin = new System.Windows.Forms.DateTimePicker();
             this.dtpFechaInicio = new System.Windows.Forms.DateTimePicker();
             this.label1 = new System.Windows.Forms.Label();
             this.VisorPDFVentas = new AxAcroPDFLib.AxAcroPDF();
-            ((System.ComponentModel.ISupportInitialize)(this.pbPDF)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.VisorPDFVentas)).BeginInit();
             this.SuspendLayout();
             // 
             // btnGenerar
             // 
-            this.btnGenerar.BackColor = System.Drawing.Color.DarkOliveGreen;
-            this.btnGenerar.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+            this.btnGenerar.BackColor = System.Drawing.Color.Sienna;
+            this.btnGenerar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnGenerar.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold);
             this.btnGenerar.ForeColor = System.Drawing.Color.White;
-            this.btnGenerar.Location = new System.Drawing.Point(561, 96);
-            this.btnGenerar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnGenerar.Location = new System.Drawing.Point(866, 42);
             this.btnGenerar.Name = "btnGenerar";
-            this.btnGenerar.Size = new System.Drawing.Size(96, 38);
+            this.btnGenerar.Size = new System.Drawing.Size(161, 35);
             this.btnGenerar.TabIndex = 17;
             this.btnGenerar.Text = "Generar";
             this.btnGenerar.UseVisualStyleBackColor = false;
             this.btnGenerar.Click += new System.EventHandler(this.btnGenerar_Click);
             // 
-            // pbPDF
-            // 
-            this.pbPDF.Image = global::LP2Rest.Properties.Resources.pngegg__1_;
-            this.pbPDF.Location = new System.Drawing.Point(563, 6);
-            this.pbPDF.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
-            this.pbPDF.Name = "pbPDF";
-            this.pbPDF.Size = new System.Drawing.Size(85, 85);
-            this.pbPDF.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pbPDF.TabIndex = 18;
-            this.pbPDF.TabStop = false;
-            // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(40, 74);
-            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Bold);
+            this.label3.Location = new System.Drawing.Point(30, 72);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(83, 16);
+            this.label3.Size = new System.Drawing.Size(150, 24);
             this.label3.TabIndex = 22;
             this.label3.Text = "Fecha de fin:";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(40, 31);
-            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Font = new System.Drawing.Font("MS UI Gothic", 18F, System.Drawing.FontStyle.Bold);
+            this.label2.Location = new System.Drawing.Point(29, 26);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(101, 16);
+            this.label2.Size = new System.Drawing.Size(180, 24);
             this.label2.TabIndex = 21;
             this.label2.Text = "Fecha de inicio:";
             // 
             // dtpFechaFin
             // 
-            this.dtpFechaFin.Location = new System.Drawing.Point(165, 69);
-            this.dtpFechaFin.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dtpFechaFin.Font = new System.Drawing.Font("Segoe UI", 14F);
+            this.dtpFechaFin.Location = new System.Drawing.Point(215, 67);
+            this.dtpFechaFin.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dtpFechaFin.Name = "dtpFechaFin";
-            this.dtpFechaFin.Size = new System.Drawing.Size(265, 22);
+            this.dtpFechaFin.Size = new System.Drawing.Size(334, 32);
             this.dtpFechaFin.TabIndex = 20;
             // 
             // dtpFechaInicio
             // 
-            this.dtpFechaInicio.Location = new System.Drawing.Point(165, 25);
-            this.dtpFechaInicio.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dtpFechaInicio.Font = new System.Drawing.Font("Segoe UI", 14F);
+            this.dtpFechaInicio.Location = new System.Drawing.Point(214, 21);
+            this.dtpFechaInicio.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dtpFechaInicio.Name = "dtpFechaInicio";
-            this.dtpFechaInicio.Size = new System.Drawing.Size(265, 22);
+            this.dtpFechaInicio.Size = new System.Drawing.Size(335, 32);
             this.dtpFechaInicio.TabIndex = 19;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(485, 33);
-            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Location = new System.Drawing.Point(364, 28);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(0, 17);
+            this.label1.Size = new System.Drawing.Size(0, 13);
             this.label1.TabIndex = 25;
             // 
             // VisorPDFVentas
             // 
             this.VisorPDFVentas.Enabled = true;
-            this.VisorPDFVentas.Location = new System.Drawing.Point(83, 149);
+            this.VisorPDFVentas.Location = new System.Drawing.Point(22, 129);
+            this.VisorPDFVentas.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.VisorPDFVentas.Name = "VisorPDFVentas";
             this.VisorPDFVentas.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("VisorPDFVentas.OcxState")));
-            this.VisorPDFVentas.Size = new System.Drawing.Size(461, 303);
+            this.VisorPDFVentas.Size = new System.Drawing.Size(1005, 488);
             this.VisorPDFVentas.TabIndex = 26;
             // 
             // frmReporteOrdenesVenta
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(851, 604);
+            this.ClientSize = new System.Drawing.Size(1054, 628);
             this.Controls.Add(this.VisorPDFVentas);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.dtpFechaFin);
             this.Controls.Add(this.dtpFechaInicio);
-            this.Controls.Add(this.pbPDF);
             this.Controls.Add(this.btnGenerar);
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "frmReporteOrdenesVenta";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Reporte de Ordenes de Venta";
-            ((System.ComponentModel.ISupportInitialize)(this.pbPDF)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.VisorPDFVentas)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -146,8 +132,6 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.PictureBox pbPDF;
         private System.Windows.Forms.Button btnGenerar;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
